afc836d760c7b1955e84a22f039e0a46d0f8bfac
naotoseki-cmind <103480047+naotoseki-cmind@users.noreply.github.com>
2022-10-26 23:32:45 +0900

    課題提出用

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,10 @@
+import javax.swing.AbstractAction;
+
+public class Task1_3 {
+    public static void main(String[] args) {
+        int hight = 3
+        System.out.println(hight);
+    }
+
+}
+    
